9e60261e778a94055dab32abd530fc7a526e43e5
MinkX <32953135+xmk2222@users.noreply.github.com>
2019-07-30 21:19:22 +0800

    Update Cplusplus_header.cpp

--- Cplusplus_header.cpp
+++ Cplusplus_header.cpp
@@ -1,7 +1,7 @@
 #include<iostream> //getline(cin, )
 #include<algorithm> // sort(a.begin(), a.end(), greater<int>()) reverse(a.begin(), a.end())
 #include<cmath> // pow(double x, double y)
-#include<limits.h> // INT_MAX, INT_MIN
+#include<climits> // INT_MAX, INT_MIN
 #include<vector> //push_back(), myvec.erase(pos_iter), myvec.erase(start_iter, end_iter)
 // pair<class T, class T>, mypair.first, mypair.second
 #include<list> //push_back(), push_front(), mylist.sort(), mylist.insert(pos_iter, ele_num, ele), mylist.erase(iter), mylist.erase(iter_first, iter_last)
